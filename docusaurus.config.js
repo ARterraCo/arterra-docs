e3b97355df801fe3f11f0be0380239d0f2510c92
Yevhenii Kuznetsov <y.kuznetsov@4irelabs.com>
2022-05-04 11:50:36 +0300

    Generated built pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://arterraco.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ARterraCo', // Usually your GitHub org/user name.
+  projectName: 'arterraco.github.io', // Usually your repo name.
+  deploymentBranch: 'dev',
+  trailingSlash: false,
 
   presets: [
     [
