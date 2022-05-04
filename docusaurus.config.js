c9cb6f3fca2ed940b4a0c832fdd0b7b98e1d6551
Yevhenii Kuznetsov <y.kuznetsov@4irelabs.com>
2022-05-04 18:57:32 +0300

    update theme background

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,7 +31,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl: 'https://github.com/ARterraCo/arterraco.github.io/tree/dev/',
           versions: {
-            current: { label: `v0.0.23-v0.0.19` },
+            current: { label: `v0.0.23-v0.0.20` },
           },
         },
         blog: {
